f5e08e18fe9660355294a8167c3af1dc8d69abc5
a787825 <thumbb69982@hotmail.com>
2023-08-14 17:02:47 +0800

    Change commit

---
+++ app/auth/facebook/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <main>
+    <h1>facebook</h1>
+
+    </main>
+  )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import '@/styles/globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Navbar, Footer } from '@/components/common'
+import Provider from '@/redux/provider'
+import { Setup } from '@/components/utils'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Hwuluduen Orchids',
+  description: 'Hwuluduen Orchids Application ',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Provider>
+            <Setup />
+            <Navbar />
+              <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 my-8'>
+                {children}
+              </div>
+            <Footer />
+        </Provider>
+      </body>
+    </html>
+  )
+}
